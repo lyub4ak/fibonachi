c755f8721e77fdf4d17cf26275ab355ffbe5903d
Denis <lyub4ak@gmail.com>
2019-09-30 14:31:19 +0300

    Added some comments.

--- task1_fibonachi.php
+++ task1_fibonachi.php
@@ -6,9 +6,11 @@ $i_sum = 0;
 
 for($j=0; $j<6; $j++){
     for ($i=0; $i<6; $i++){
+        // First two elements are already filled.
         if($j===0 && $i<2)
             continue;
 
+        // Fills array.
         $i_term_a = $i-2 >= 0 ? $a_fibonachi[$i-2][$j] : $a_fibonachi[5+$i-1][$j-1];
         $i_term_b = $i-1 >= 0 ? $a_fibonachi[$i-1][$j] : $a_fibonachi[5][$j-1];
         $a_fibonachi[$i][$j] = $i_term_a + $i_term_b;
